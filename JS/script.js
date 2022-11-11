4a9d883ddd01405aa80cf9d42d05b769722fecb4
Valerie <valdorisme@yahoo.com>
2022-11-11 12:22:02 -0500

    Attempting to add an event listener to save button

--- JS/script.js
+++ JS/script.js
@@ -2,23 +2,36 @@ var timeCurrent = moment().hour();
 console.log(moment());
 
 var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var saveSelection = document.getElementById('.saveBtn');
+var saveSelection = document.querySelectorAll('.saveBtn');
 
 
 // Setting classes/functionality for past, present, and future
 document.querySelector('#currentDay').textContent = moment().format("dddd, MMMM Do YYYY");
-for (var i=0; i <9; i++) {
+for (var i=9; i <17; i++) {
     if (timeCurrent === hours[i]) {
         console.log(i);
         document.getElementById(i).classList.add("present");
     }
-    if (timeCurrent < hours[i]) {
-        console.log(i);
-        document.getElementById(i).classList.add("future");
-    }
-    if (timeCurrent > hours[i]) {
+
+    else if (timeCurrent > hours[i]) {
         console.log(i);
         document.getElementById(i).classList.add("past");
     }
+
+    else if (timeCurrent < hours[i]) {
+        console.log(i);
+        document.getElementById(i).classList.add("future");
+    }
 }
 
+if (localStorage.getItem(i)) {
+    document.getElementById(i).value = localStorage.getItem(i);
+}
+
+saveSelection[i].addEventListener("click", saveButton());
+
+function saveButton() {
+   var num = this.getAttribute("col-id") ;
+   var id = document.getElementById(id).value;
+    localStorage.setItem(num, id);
+}
