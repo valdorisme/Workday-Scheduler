7c0321d9708bd227ca110f83476c609e53640e45
Valerie <valdorisme@yahoo.com>
2022-11-11 06:26:04 -0500

    Repository changed. Previous code was not pushing through to repository. Code includes HTML and Javascript changes

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+// var timeCurrent = moment
+console.log(moment());
+
+// var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+// var saveSelection = document.getElementById('.saveBtn');
+
+// document.getElementById('#currentDay').textContent = 
+// moment().format("dd, MMMM, Do, YYYY");
